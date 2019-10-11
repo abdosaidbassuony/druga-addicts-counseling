b5fdbe1032f25540080762f70220120a18b88cbd
abdosaidbassuony <32787779+abdosaidbassuony@users.noreply.github.com>
2019-10-11 06:19:18 +0200

    testing commit

---
+++ app/src/main/java/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.drugaddictscounselingsystem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
